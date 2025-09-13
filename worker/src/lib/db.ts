5dab4c30e2f9feb58b631678f3871d2027ab7c36
Dey11 <shreyandey11@gmail.com>
2025-09-14 00:49:18 +0530

    fix singleton prisma client

--- worker/src/lib/db.ts
+++ worker/src/lib/db.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+let prisma: PrismaClient;
+
+declare global {
+  var prisma: PrismaClient;
+}
+
+if (typeof globalThis.prisma === "undefined") {
+  prisma = new PrismaClient();
+  globalThis.prisma = prisma;
+} else {
+  prisma = globalThis.prisma;
+}
+export default prisma;
